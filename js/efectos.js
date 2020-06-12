d0beb962445ce47ae2d4bbf8b9959d7e4d9e9105
Arthur-Escanor <64905903+Arthur-Escanor@users.noreply.github.com>
2020-06-12 16:32:51 -0500

    preparando archivos

---
+++ js/efectos.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+//Efecto menu    
+   $('.menu a').each(function(index, elemento){
+       $(this).css({
+           'top': '-200px'
+        });
+
+       $(this).animate({
+           top: '0'
+        }, 2000 + (index * 500 ));
+    }); 
+
+//Efectos Header
+    if($(window).width() > 800) {
+        $('header .textos') .css({
+            opacity: 0,
+            marginTop: 0
+        });
+
+        $('header .textos').animate({
+            opacity: 1,
+            marginTop: '-52px'
+        }, 1500);
+    }
+
+// Scroll Elementos Menu
+    var acercaDe = $('#acerca-de').offset().top;
+        menu = $('#acerca-de').offset().top,
+        galeria = $('#galeria').offset().top,
+        ubicacion = $('#ubicacion').offset().top;
+    
+     $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('hmtml, body').animate({
+            scrollTop: acercaDe - 100
+        }, 500);
+    });
+
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('hmtml, body').animate({
+            scrollTop: acercaDe - 100
+        }, 500);
+    });
+
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('hmtml, body').animate({
+            scrollTop: acercaDe - 100
+        }, 500);
+    });
+
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('hmtml, body').animate({
+            scrollTop: acercaDe - 100
+        }, 500);
+    });
+
+});    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
